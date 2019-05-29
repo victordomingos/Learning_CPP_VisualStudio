4e00d78aa6c089ada9714a221b5743b9caaf3db0
victordomingos <editor.arcosonline@gmail.com>
2019-05-29 22:51:23 +0100

    polish

--- PRODUCAO_1/ProjCLR/Form1.h
+++ PRODUCAO_1/ProjCLR/Form1.h
@@ -86,6 +86,7 @@ namespace ProjCLR {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->txt_qtd = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -108,151 +109,93 @@ namespace ProjCLR {
 			// 
 			// txt_qtd
 			// 
-			this->txt_qtd->Location = System::Drawing::Point(11, 39);
+			resources->ApplyResources(this->txt_qtd, L"txt_qtd");
 			this->txt_qtd->Name = L"txt_qtd";
-			this->txt_qtd->Size = System::Drawing::Size(85, 20);
-			this->txt_qtd->TabIndex = 0;
-			this->txt_qtd->Text = L"1";
-			this->txt_qtd->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
 			this->txt_qtd->TextChanged += gcnew System::EventHandler(this, &Form1::Txt_qtd_TextChanged);
 			// 
 			// label1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(12, 20);
+			resources->ApplyResources(this->label1, L"label1");
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(62, 13);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"Quantidade";
 			// 
 			// label2
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(125, 20);
+			resources->ApplyResources(this->label2, L"label2");
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
-			this->label2->TabIndex = 2;
-			this->label2->Text = L"Preço";
 			// 
 			// label3
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(12, 82);
+			resources->ApplyResources(this->label3, L"label3");
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(45, 13);
-			this->label3->TabIndex = 3;
-			this->label3->Text = L"Escalão";
 			// 
 			// label6
 			// 
-			this->label6->AutoSize = true;
-			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label6->Location = System::Drawing::Point(11, 257);
+			resources->ApplyResources(this->label6, L"label6");
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(63, 20);
-			this->label6->TabIndex = 6;
-			this->label6->Text = L"TOTAL:";
 			// 
 			// txt_total
 			// 
-			this->txt_total->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->txt_total->Location = System::Drawing::Point(11, 280);
+			resources->ApplyResources(this->txt_total, L"txt_total");
 			this->txt_total->Name = L"txt_total";
-			this->txt_total->Size = System::Drawing::Size(215, 44);
-			this->txt_total->TabIndex = 8;
-			this->txt_total->Text = L"0";
-			this->txt_total->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
 			// 
 			// txt_preco
 			// 
-			this->txt_preco->Location = System::Drawing::Point(122, 39);
+			resources->ApplyResources(this->txt_preco, L"txt_preco");
 			this->txt_preco->Name = L"txt_preco";
-			this->txt_preco->Size = System::Drawing::Size(104, 20);
-			this->txt_preco->TabIndex = 9;
-			this->txt_preco->Text = L"10";
-			this->txt_preco->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
 			this->txt_preco->TextChanged += gcnew System::EventHandler(this, &Form1::Txt_preco_TextChanged);
 			// 
 			// combo_escalao
 			// 
 			this->combo_escalao->FormattingEnabled = true;
-			this->combo_escalao->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Menor", L"Adulto", L"Reformado" });
-			this->combo_escalao->Location = System::Drawing::Point(11, 99);
+			this->combo_escalao->Items->AddRange(gcnew cli::array< System::Object^  >(3) {
+				resources->GetString(L"combo_escalao.Items"),
+					resources->GetString(L"combo_escalao.Items1"), resources->GetString(L"combo_escalao.Items2")
+			});
+			resources->ApplyResources(this->combo_escalao, L"combo_escalao");
 			this->combo_escalao->Name = L"combo_escalao";
-			this->combo_escalao->Size = System::Drawing::Size(216, 21);
-			this->combo_escalao->TabIndex = 10;
-			this->combo_escalao->Text = L"Clique para selecionar...";
 			this->combo_escalao->SelectedIndexChanged += gcnew System::EventHandler(this, &Form1::Combo_escalao_SelectedIndexChanged);
 			// 
 			// chk_x
 			// 
-			this->chk_x->AutoSize = true;
-			this->chk_x->Location = System::Drawing::Point(17, 21);
+			resources->ApplyResources(this->chk_x, L"chk_x");
 			this->chk_x->Name = L"chk_x";
-			this->chk_x->Size = System::Drawing::Size(33, 17);
-			this->chk_x->TabIndex = 11;
-			this->chk_x->Text = L"X";
 			this->chk_x->UseVisualStyleBackColor = true;
 			this->chk_x->CheckedChanged += gcnew System::EventHandler(this, &Form1::Chk_x_CheckedChanged);
 			// 
 			// chk_y
 			// 
-			this->chk_y->AutoSize = true;
-			this->chk_y->Location = System::Drawing::Point(17, 45);
+			resources->ApplyResources(this->chk_y, L"chk_y");
 			this->chk_y->Name = L"chk_y";
-			this->chk_y->Size = System::Drawing::Size(33, 17);
-			this->chk_y->TabIndex = 12;
-			this->chk_y->Text = L"Y";
 			this->chk_y->UseVisualStyleBackColor = true;
 			this->chk_y->CheckedChanged += gcnew System::EventHandler(this, &Form1::Chk_y_CheckedChanged);
 			// 
 			// chk_z
 			// 
-			this->chk_z->AutoSize = true;
-			this->chk_z->Location = System::Drawing::Point(17, 69);
+			resources->ApplyResources(this->chk_z, L"chk_z");
 			this->chk_z->Name = L"chk_z";
-			this->chk_z->Size = System::Drawing::Size(33, 17);
-			this->chk_z->TabIndex = 13;
-			this->chk_z->Text = L"Z";
 			this->chk_z->UseVisualStyleBackColor = true;
 			this->chk_z->CheckedChanged += gcnew System::EventHandler(this, &Form1::Chk_z_CheckedChanged);
 			// 
 			// rad_sim
 			// 
-			this->rad_sim->AutoSize = true;
-			this->rad_sim->Location = System::Drawing::Point(17, 21);
+			resources->ApplyResources(this->rad_sim, L"rad_sim");
 			this->rad_sim->Name = L"rad_sim";
-			this->rad_sim->Size = System::Drawing::Size(42, 17);
-			this->rad_sim->TabIndex = 14;
 			this->rad_sim->TabStop = true;
-			this->rad_sim->Text = L"Sim";
 			this->rad_sim->UseVisualStyleBackColor = true;
 			this->rad_sim->CheckedChanged += gcnew System::EventHandler(this, &Form1::Rad_sim_CheckedChanged);
 			// 
 			// rad_nao
 			// 
-			this->rad_nao->AutoSize = true;
-			this->rad_nao->Location = System::Drawing::Point(17, 45);
+			resources->ApplyResources(this->rad_nao, L"rad_nao");
 			this->rad_nao->Name = L"rad_nao";
-			this->rad_nao->Size = System::Drawing::Size(45, 17);
-			this->rad_nao->TabIndex = 15;
 			this->rad_nao->TabStop = true;
-			this->rad_nao->Text = L"Não";
 			this->rad_nao->UseVisualStyleBackColor = true;
 			this->rad_nao->CheckedChanged += gcnew System::EventHandler(this, &Form1::Rad_nao_CheckedChanged);
 			// 
 			// label7
 			// 
-			this->label7->AutoSize = true;
-			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label7->Location = System::Drawing::Point(99, 39);
+			resources->ApplyResources(this->label7, L"label7");
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(21, 25);
-			this->label7->TabIndex = 16;
-			this->label7->Text = L"*";
 			this->label7->Click += gcnew System::EventHandler(this, &Form1::Label7_Click);
 			// 
 			// groupBox1
@@ -260,29 +203,22 @@ namespace ProjCLR {
 			this->groupBox1->Controls->Add(this->chk_x);
 			this->groupBox1->Controls->Add(this->chk_y);
 			this->groupBox1->Controls->Add(this->chk_z);
-			this->groupBox1->Location = System::Drawing::Point(11, 136);
+			resources->ApplyResources(this->groupBox1, L"groupBox1");
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(85, 94);
-			this->groupBox1->TabIndex = 17;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Extras";
 			// 
 			// groupBox2
 			// 
 			this->groupBox2->Controls->Add(this->rad_sim);
 			this->groupBox2->Controls->Add(this->rad_nao);
-			this->groupBox2->Location = System::Drawing::Point(122, 136);
+			resources->ApplyResources(this->groupBox2, L"groupBox2");
 			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->Size = System::Drawing::Size(105, 94);
-			this->groupBox2->TabIndex = 18;
 			this->groupBox2->TabStop = false;
-			this->groupBox2->Text = L"Agravamento";
 			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(239, 336);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->label7);
@@ -298,7 +234,6 @@ namespace ProjCLR {
 			this->MaximizeBox = false;
 			this->Name = L"Form1";
 			this->SizeGripStyle = System::Windows::Forms::SizeGripStyle::Hide;
-			this->Text = L"Simulação de Tarifa";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
@@ -309,12 +244,9 @@ namespace ProjCLR {
 
 		}
 #pragma endregion
-    private: System::Void Label7_Click(System::Object^ sender, System::EventArgs^ e) {
-    }
+private: System::Void Label7_Click(System::Object^ sender, System::EventArgs^ e) { }
 private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) { calcular_total(); }
 private: System::Void Btn_calcular_Click(System::Object^ sender, System::EventArgs^ e) { calcular_total(); }
-
-
 private: System::Void Txt_qtd_TextChanged(System::Object^ sender, System::EventArgs^ e) { calcular_total(); }
 private: System::Void Txt_preco_TextChanged(System::Object^ sender, System::EventArgs^ e) { calcular_total(); }
 private: System::Void Combo_escalao_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) { calcular_total(); }
